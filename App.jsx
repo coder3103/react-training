151675e83eeb39e313bb046a35fd0239a365a149
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-11-30 21:27:10 +0700

    1 file modified

--- App.jsx
+++ App.jsx
@@ -2,37 +2,38 @@ import React from 'react'
 import "./App.css";
 import { useEffect, useState } from "react";
 
-const withMousePosition = (WrappedComponent) => {
-  return (props) => {
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
+/*
 
-  useEffect(() => {
-    const handleMousePositionChange = (e) => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
-    };
+1. create a HOR called withMousePosition and takes in commponent 
+params called WrappedComponent
 
-    window.addEventListener("mousemove", handleMousePositionChange);
+2. inside the HOC, return (props) => {} and inside
+include a state called mousePosition which holds an object with x: 0, y:0
 
-    return () => {
-      window.removeEventListener("mousemove", handleMousePositionChange);
-    };
-  }, []);
+3. still inside the return, create a useeEffect that includes
+a function called handleMousePositionChange with param e and will
+set mousePosition state to x: e.clientX, y: e.clientY
 
-  return <WrappedComponent {...props} mousePosition={mousePosition}/>;
-  };
-};
+4. still inside the useEffect, create a window.addEventListener() and
+includes param "mousemove" and the function handleMousePositionChange;
 
-const PanelMouseLogger = ({mousePosition}) => {
-  if(!mousePosition) {
-    return null;
-  }
-  return (
+5. below number 4, write a return () => {} and removes the event listener 
+from number 4 and add an empty dependency for the useEffect
+
+6. after the useEffect and still inside the withMousePosition function, 
+return <WrappedComponent {...props} mousePosition={mousePosition} />
+
+*/
+
+/*
+
+7. create a new function component called PanelMouseLoger and recieves a
+prop called mousePosition
+
+8. inside the component, if the mousePosition is not true, return null
+
+9. below the if, return 
+
     <div className="BasicTracker">
       <p>Mouse position:</p>
           <div className="Row">
@@ -40,29 +41,34 @@ const PanelMouseLogger = ({mousePosition}) => {
             <span>y: {mousePosition.y}</span>
           </div>
     </div>
-  );
-};
 
-const PointMouseLogger = ({mousePosition}) => {
-    if(!mousePosition) {
-    return null;
-  }
-  return (
+*/
+
+/*
+
+10. create a new function component called PointMouseLogger and recieves a
+prop called mousePosition
+
+11. inside the component, if the mousePosition is not true, return null
+
+12. below the if, return 
+
         <p>
           ({mousePosition.x}, {mousePosition.y})
         </p>
-  );
-};
 
-const PanelMouseTracker = withMousePosition(PanelMouseLogger)
-const PointMouseTracker = withMousePosition(PointMouseLogger)
+
+13. below the component, pass the two function components above into
+the HOC withMousePosition and save each one to a new const variable 
+
+14. add those new enhanced components into the App component under the <header>
+
+*/
 
 function App() {
   return (
     <div className="App">
       <header className="Header">Little Lemon Restaurant 🍕</header>
-      <PanelMouseTracker />
-      <PointMouseTracker />
     </div>
   );
 }
