a4460eb17d2fa468c305a88ff18a3ba030f4375d
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 06:45:34 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,41 +1,7 @@
 import * as React from 'react';
 import { useState } from "react";
 
-function App() {
-  const [userInput, setUserInput] = useState("");
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState(false);
-
-  return (
-    <div>
-      <main className="App" style={{ color: color ? "#FFF" : "#FFF952" }}>
-        <input
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-        />
-        <br />
-        <br />
-        <p>{count}</p>
-        <section>
-          <button onClick={() => setCount((prev) => prev - 1)}>-</button>
-          <button onClick={() => setCount((prev) => prev + 1)}>-</button>
-          <button onClick={() => setColor((prev) => !prev)}>Color</button>
-        </section>
-        <br />
-        <br />
-        <p>{userInput}</p>
-      </main>
-    </div>
-  );
-}
-
-export default App;
-
-/*
-
-- convert all useStates to the useReducer
-- use ACTION object below
+import { useReducer } from 'react';
 
 const ACTION = {
   INCREMENT: 'increment',
@@ -44,7 +10,42 @@ const ACTION = {
   TG_COLOR: 'tgColor'
 }
 
-solution:
-https://github.com/gitdagray/usereducer_tut/blob/main/src/App.js
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case ACTION.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case ACTION.NEW_USER_INPUT:
+      return { ...state, userInput: action.payload };
+    case ACTION.TG_COLOR:
+      return { ...state, color: !state.color };
+    default:
+      throw new Error();
+  }
+}
 
-*/
+function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, userInput: '', color: false })
+
+  return (
+    <main className="App" style={{ color: state.color ? '#FFF' : '#FFF952' }}>
+      <input
+        type="text"
+        value={state.userInput}
+        onChange={(e) => dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })}
+      />
+      <br /><br />
+      <p>{state.count}</p>
+      <section>
+        <button onClick={(() => dispatch({ type: ACTION.DECREMENT }))}>-</button>
+        <button onClick={(() => dispatch({ type: ACTION.INCREMENT }))}>+</button>
+        <button onClick={(() => dispatch({ type: ACTION.TG_COLOR }))}>Color</button>
+      </section>
+      <br /><br />
+      <p>{state.userInput}</p>
+    </main>
+  );
+}
+
+export default App;
