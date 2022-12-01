3f9263e5c1fd32087353cea71e0de90c1fe2269b
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-01 17:11:36 +0700

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
 import * as React from 'react';
-import './style.css';
+import { useState, useEffect, useRef } from "react";
+import './App.css'
 
 export default function App() {
+  const [day, setDay] = useState("Monday");
+  const prevDay = usePrevious(day);
+  const getNextDay = () => {
+    if (day === "Monday") {
+      setDay("Tuesday")
+    } else if (day === "Tuesday") {
+      setDay("Wednesday")
+    } else if (day === "Wednesday") {
+      setDay("Thursday")
+    } else if (day === "Thursday") {
+      setDay("Friday")
+    } else if (day === "Friday") {
+      setDay("Monday")
+    }
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{padding: "40px"}}>
+      <h1>
+        Today is: {day}<br />
+        {
+          prevDay && (
+            <span>Previous work day was: {prevDay}</span>
+          )
+        }
+      </h1>
+      <button onClick={getNextDay}>
+        Get next day
+      </button>
     </div>
   );
+}
+function usePrevious(val) {
+  const ref = useRef(null)
+  useEffect(() => {
+    ref.current = val;
+  }), [val]
+
+  return ref.current
 }
