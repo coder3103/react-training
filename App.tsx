b1f2335b9f03c9c7e50a887a9ca7f71e973da1be
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 06:43:33 +0700

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
 import * as React from 'react';
-import './style.css';
+import { useState } from "react";
 
-export default function App() {
+function App() {
+  const [userInput, setUserInput] = useState("");
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(false);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <main className="App" style={{ color: color ? "#FFF" : "#FFF952" }}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <br />
+        <br />
+        <p>{count}</p>
+        <section>
+          <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+          <button onClick={() => setCount((prev) => prev + 1)}>-</button>
+          <button onClick={() => setColor((prev) => !prev)}>Color</button>
+        </section>
+        <br />
+        <br />
+        <p>{userInput}</p>
+      </main>
     </div>
   );
 }
+
+export default App;
+
+/*
+
+- convert all useStates to the useReducer
+- use ACTION object below
+
+const ACTION = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+  NEW_USER_INPUT: 'newUserInput',
+  TG_COLOR: 'tgColor'
+}
+
+solution:
+https://github.com/gitdagray/usereducer_tut/blob/main/src/App.js
+
+*/
