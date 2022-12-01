bb5713ceb26ad92d632e1c24cc183118a8580d82
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-01 17:04:28 +0700

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
-import './style.css';
+import './App.css'
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+function App() {
+  const [user, setUser] = React.useState([]);
+
+  const fetchData = () => {
+    fetch("https://randomuser.me/api/?results=1")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+    <div style={{padding: "40px"}}>
+      <h1>Customer data</h1>
+      <h2>Name: {user.results[0].name.first}</h2>
+      <img src={user.results[0].picture.large} alt="" />
     </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
+
+export default App;
