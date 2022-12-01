6302eadb2f13b803cdaa1975e2b24e4a065811bc
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-01 16:14:48 +0700

    1 file modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
-import './style.css';
+import "./App.css";
+import DessertsList from "./DessertsList";
 
-export default function App() {
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+];
+
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
     </div>
   );
 }
+
+export default App;
---
+++ DessertsList.js
@@ -0,0 +1,15 @@
+function DessertsList(props) {
+  // Implement the component here.
+
+  const list = props.data.filter(food => {
+    return food.calories < 500
+  }).sort((a, b) => {
+    return a.calories - b.calories;
+  }).map(food => {
+    return <li>{`${food.name} - ${food.calories} cal`}</li>
+  })
+
+  return <ul>{list}</ul>;
+}
+
+export default DessertsList;
