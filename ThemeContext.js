de6515a4eefa7613719fa34c242796cee7d91ab8
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-01 16:41:27 +0700

    1 file modified, 4 added

---
+++ ThemeContext.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext(undefined);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme: () => setTheme(theme === "light" ? "dark" : "light"),
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
+
