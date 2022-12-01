72a81c339ff0c6b363b023aef7cb3c81f1546d3d
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 06:31:11 +0700

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,67 @@
 import * as React from 'react';
-import './style.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-export default function App() {
+const MousePosition = ({ render }) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const handleMousePositionChange = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMousePositionChange);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMousePositionChange);
+    };
+  }, []);
+
+  return render({ mousePosition });
+};
+
+const PanelMouseLogger = () => {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="BasicTracker">
+      <p>Mouse position:</p>
+      <MousePosition
+        render={({ mousePosition }) => (
+          <div className="Row">
+            <span>x: {mousePosition.x}</span>
+            <span>y: {mousePosition.y}</span>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+
+const PointMouseLogger = () => {
+  return (
+    <MousePosition
+      render={({ mousePosition }) => (
+        <p>
+          ({mousePosition.x}, {mousePosition.y})
+        </p>
+      )}
+    />
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseLogger />
+      <PointMouseLogger />
     </div>
   );
 }
+
+export default App;
