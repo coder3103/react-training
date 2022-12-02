86136c44cf5ccc8f6ee557fe531779360b84493b
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 12:15:19 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
+import { useMemo, useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState(0);
+
+  const expensiveFunction = (n) => {
+    console.log("function running!");
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+      total += i;
+    }
+    return total;
+  };
+
+  const sum = useMemo(() => expensiveFunction(number), [number]);
+
+  console.log("component rendered!");
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <input
+        onChange={(e) => setText(e.target.value)}
+        placeholder="enter a text"
+      />
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <span>Total: {sum}</span>
     </div>
   );
 }
+
+export default App;
