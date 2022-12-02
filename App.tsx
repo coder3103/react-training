01391f73de256fe0bd96215751ecf891329cd604
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 07:37:51 +0700

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import Navbar from './Navbar'
 
 function App() {
--- Navbar.js
+++ Navbar.js
@@ -1,8 +1,10 @@
 import './Navbar.css'
 import { useTheme } from './useTheme';
 import * as React from 'react';
+import { useTheme } from './useTheme';
 
 const Navbar = () => {
+    const {color} = useTheme()
     return ( 
         <div className="navbar">
             <nav>
