f5e2ffdfb910abb92aa21b3aefdd7d2f1f14a951
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 14:21:19 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -9,21 +9,21 @@ const App = () => {
     const [num1] = useState(4);
     const [num2] = useState(5);
 
-    const sum = useCallback(() => num1 + num2, [num1, num2]);
+    //const sum = useCallback(() => num1 + num2, [num1, num2]);
+    const buildArray = useCallback(() => [num1, num2], [num1, num2]);
 
     useEffect(() => {
-        console.log(`New sum: ${sum()}`);
-        setResult(sum())
-    }, [sum]);
+        console.log(`New array: ${buildArray()}`);
+        setResult(buildArray());
+    }, [buildArray]);
 
     return (
         <main className="App">
             <input type="text" placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
             <h1>Output: {userInput || "--"}</h1>
-            <h2>{result}</h2>
+            <p>Result: {JSON.stringify(result)}</p>
         </main>
     )
 }
-
 
 export default App
