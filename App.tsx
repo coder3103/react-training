c1c7b704b4e4246b6d777adf11e17aad4c8b7fc4
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 10:51:52 +0700

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
 import * as React from 'react';
-import './style.css';
+import { useState, useEffect, useMemo, useCallback } from 'react';
+import './App.css'
 
-export default function App() {
+const getArray = () => {
+  for (let i = 0; i < 1000000000; i++) {
+    //do something expensive
+  }
+  return ['Dave', 'Gray'];
+}
+
+function App() {
+  const [userNumber, setUserNumber] = useState('');
+  const [randomInput, setRandomInput] = useState('');
+
+  const fib = useCallback((n) => {
+    return (n <= 1) ? n : fib(n - 1) + fib(n - 2);
+  }, [])
+
+  const fibNumber = useMemo(() => fib(userNumber), [userNumber, fib]);
+
+  const myArray = useMemo(() => getArray(), []);
+
+  useEffect(() => {
+    console.log('New array')
+  }, [myArray])
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <main className="App">
+      <label>Fibonacci Sequence:</label>
+      <input type="number" value={userNumber} placeholder="Position" onChange={(e) => setUserNumber(e.target.value)} />
+      <p>Number: {fibNumber || "--"}</p>
+      <br /><br />
+      <label>Random Input:</label>
+      <input type="text" value={randomInput} placeholder="Random Input" onChange={(e) => setRandomInput(e.target.value)} />
+      <p>{randomInput}</p>
+    </main>
   );
 }
+
+export default App;
