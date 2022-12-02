0ead4d6b90272655d88955a63d7b22f327f724fa
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 11:19:53 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -13,17 +13,13 @@ function App() {
   const [userNumber, setUserNumber] = useState('');
   const [randomInput, setRandomInput] = useState('');
 
-  const fib = useCallback((n) => {
+  const fib = (n) => {
     return (n <= 1) ? n : fib(n - 1) + fib(n - 2);
-  }, [])
+  }
 
   const fibNumber = fib(userNumber);
 
   const myArray = getArray();
-
-  useEffect(() => {
-    console.log('New array')
-  }, [myArray])
 
   return (
     <main className="App">
@@ -39,3 +35,21 @@ function App() {
 }
 
 export default App;
+
+/*
+
+1. notice how slow it is when tryping in position or random input
+
+2. the first thing to do is to add a useEffect with fibNumber dependancy in the App component
+
+3. wrap the useMemo hook around the fib(userNumber) that is just before the const fibNumber and add a dependancy of userNumber and fib
+
+4. wrap the useCallBack over the (n) => {} function with an empty dependancy and remember the useCallBack is to memonize a function and useMemo is for results
+
+5. now remove fibNumber from the useEffect dependancy and add myArray instead and console.log a message in the useEffect and check the console (every rerender will run the expensive loop)
+
+6. apply useMemo to the getArray function with an empty dependancy for the const myArray
+
+7. useMemo allows for referential equality, which means [] === [] or {} === {} is true with useMemo but false without useMemo
+
+*/
