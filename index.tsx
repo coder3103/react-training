61506881ab6bd7799a0256ccc1eb1b6b6374e69b
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 07:34:53 +0700

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from './ThemeContext'
 
 import App from './App';
 
@@ -9,6 +10,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+      <ThemeProvider>
     <App />
+    </ThemeProvider>
   </StrictMode>
 );
