717e98a1ea335a7008de0ec69e333115b0297274
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 11:35:37 +0700

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import { useFetch } from "./useFetch";
 import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const { data } = useFetch(
+    "https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json"
+  );
+
+  function computeLongestWord() {
+    if (!data) {
+      return [];
+    }
+  
+    console.log("computing longest word");
+  
+    let longestWord = "";
+  
+    JSON.parse(data).forEach(sentence =>
+      sentence.split(" ").forEach(word => {
+        if (word.length > longestWord.length) {
+          longestWord = word;
+        }
+      })
+    );
+  
+    return longestWord;
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div>count: {count}</div>
+      <button onClick={()=> setCount(count + 2)}>Increment</button>
+      <div>{computeLongestWord}</div>
     </div>
   );
 }
