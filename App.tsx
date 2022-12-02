9863c9c4056a402894104b58e84d12b806a0739e
coder3103 <112038573+coder3103@users.noreply.github.com>
2022-12-02 07:02:52 +0700

    1 file modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
 import * as React from 'react';
-import './style.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Index } from "./pages/index";
+import { About } from "./pages/about";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact element={<Index/>} />
+          <Route path="/about" exact element={<About/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+export default App;
+
+/*
+
+- create a context that will show a message in the components it has wrapped around (pass a value to the context provider)
+- then instead of the static value, use a useState so that the components have access to the useState
+and are able to change the state using a button and it will affect it globally (for the value, pass an object value, setValue )
+
+video: https://www.youtube.com/watch?v=lhMKvyLRWo0
+github: https://github.com/benawad/react-hooks-tutorial/blob/7_useContext/src/pages/about.js
+
+*/
